d716799945eb4d8761bbefe6620c26cb0aaa31bf
s33147 <s33147@pjwstk.edu.pl>
2026-03-25 23:29:58 +0100

    feat: logging

--- src/Services/DeviceService.cs
+++ src/Services/DeviceService.cs
@@ -1,14 +1,17 @@
 using Tmp.Data;
 using Tmp.Models;
+using Tmp.Services.Logging;
 
 namespace Tmp.Services;
 
-public class DeviceService {
+public class DeviceService(ILogger logger) {
 	private readonly Dictionary<Guid, Device> m_devices = new();
+	private readonly ILogger m_logger = logger;
 
 	public DeviceDto AddDevice(DeviceCreator deviceData) {
 		var device = deviceData();
 		m_devices.Add(device.Id, device);
+		m_logger.LogTrace($"Device {device.Name} created");
 
 		return device.DTO();
 	}
--- src/Services/Logging/LoggerConsole.cs
+++ src/Services/Logging/LoggerConsole.cs
@@ -2,6 +2,6 @@ namespace Tmp.Services.Logging;
 
 public class LoggerConsole : ILogger {
 	public void Log(LogLevel level, object obj) {
-		Console.WriteLine($"[{level switch { LogLevel.TRACE => "TRC", LogLevel.INFO => "INF" }}] {obj}");
+		Console.WriteLine($"[{level switch { LogLevel.TRACE => "TRC", LogLevel.INFO => "INF", _ => "?" }}] {obj}");
 	}
 }
