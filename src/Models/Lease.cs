5983dee19c84c2896e1a33c0793ff7f7e5c6b3a3
s33147 <s33147@pjwstk.edu.pl>
2026-03-25 23:25:49 +0100

    feat(models): lease

--- src/Models/Lease.cs
+++ src/Models/Lease.cs
@@ -1,3 +1,15 @@
+using Tmp.Data;
+
 namespace Tmp.Models;
 
-public record class Lease(User user, Device device, DateTime start, DateTime? end);
+public class Lease(UserDto user, DeviceDto device) {
+	public Guid Id {get; set;} = Guid.NewGuid();
+	public UserDto User {get; init;} = user;
+	public DeviceDto Device {get; init;} = device;
+	public DateTime Start {get; init;}
+	public DateTime PlannedReturn {get; init;}
+	public DateTime? Return {get; set;}
+	public int Penalty {get; set;}
+
+	public LeaseDto DTO() => new LeaseDto(Id);
+}
