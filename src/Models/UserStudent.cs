f24b64783f8f2c9a8556c9f80312e7ed15091b3e
s33147 <s33147@pjwstk.edu.pl>
2026-03-25 23:55:25 +0100

    feat(models): implement limits

--- src/Models/UserStudent.cs
+++ src/Models/UserStudent.cs
@@ -2,5 +2,5 @@ namespace Tmp.Models;
 
 public class UserStudent(string firstName, string lastName, string studentNumber) : User(firstName, lastName) {
 	public string StudentNumber {get; private set;} = studentNumber;
-	public override int LeaseMax => throw new NotImplementedException();
+	public override int LeaseMax => 3;
 }
